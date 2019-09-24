bd572caffd2ebebbd7ec8ec89e2a982dbf6f2b3e
anshuln <anshulnasery@gmail.com>
2019-09-25 01:56:54 +0530

    Cleans up and vectorizes impl

--- data_conversions/prepare_cloud_dataset.py
+++ data_conversions/prepare_cloud_dataset.py
@@ -55,7 +55,7 @@ def get_mask_hue(img,k=5,l=3,string='NewMask1'):
 # def probabilistic_dist_image(img,num_points):
 
 if __name__ == "__main__":
-	batch_size = 10#int(sys.argv[1])
+	batch_size = 100#int(sys.argv[1])
 	point_num = 5000#int(sys.argv[2])
 	folder = './test_images'
 	data = np.zeros((batch_size, point_num, 6))
@@ -68,19 +68,24 @@ if __name__ == "__main__":
 			i = cv2.imread(os.path.join(dirname, filename))
 			new_image = get_mask_hue(i,k=5)
 			# print(new_image.shape)
-			points = []
-			pixels = []	
-			for x in range(new_image.shape[0]):
-				for y in range(new_image.shape[1]):
-					# print(new_image[x,y].shape)
-					if(new_image[x,y,0] + new_image[x,y,1] + new_image[x,y,2]==0):
-						continue
-					points.append((x, np.random.random() * 1e-6, y))
-					pixels.append(new_image[x,y])					
+			# points = []
+			# pixels = []	
+			# for x in range(new_image.shape[0]):
+			# 	for y in range(new_image.shape[1]):
+			# 		# print(new_image[x,y].shape)
+			# 		if(new_image[x,y,0] + new_image[x,y,1] + new_image[x,y,2]==0):
+			# 			continue
+			# 		points.append((x, np.random.random() * 1e-6, y))
+			# 		pixels.append(new_image[x,y])					
+			points = np.nonzero(new_image.sum(axis=2))
+			# print(points[0].shape)
+			pixels = new_image[[points[0],points[1]]].reshape((len(points[0]),3))
+			points = np.concatenate([points[0].reshape((len(points[0]),1)),np.random.random((len(points[0]),1)) * 1e-6,points[1].reshape((len(points[0]),1))],axis=1)
+			# print(points.shape)
 			point_num_total = point_num_total + len(points)
 			pixels_sum = np.sum(np.array(pixels))
 			probs = (np.array(pixels)/pixels_sum).sum(axis=1)
-			print(probs.shape)
+			# print(probs.shape)
 			# probs = [pixel / pixels_sum for pixel in pixels]
 			indices = np.random.choice(list(range(len(points))), size=point_num,
 									   replace=(len(points) < point_num), p=probs)
@@ -93,8 +98,8 @@ if __name__ == "__main__":
 			if ((idx_img + 1) % batch_size == 0):# or idx_img == len(images) - 1:
 				item_num = idx_in_batch + 1
 				filename_h5 = os.path.join(os.path.dirname(folder), '%d.h5' % (idx_h5))
-				print('Saving {}...'.format(filename_h5))
-				filelist_h5.write('./%s_%d.h5\n' % (tag, idx_h5))
+				print('Saving {}...'.format( filename_h5))
+				# filelist_h5.write('./%d.h5\n' % (idx_h5))
 
 				file = h5py.File(filename_h5, 'w')
 				file.create_dataset('data', data=data[0:item_num, ...])
